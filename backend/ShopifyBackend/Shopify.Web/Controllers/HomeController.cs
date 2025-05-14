6bc4eca390a8c7e3b502fcfebd2ec17f3321c8b2
Vamsi <21a91a05g5@aec.edu.in>
2025-05-14 11:37:33 +0530

    Chore: Cleaned The Project

--- backend/ShopifyBackend/Shopify.Web/Controllers/HomeController.cs
+++ backend/ShopifyBackend/Shopify.Web/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Shopify.Web.Models;
 using System.Diagnostics;
 
 namespace Shopify.Web.Controllers
@@ -16,17 +15,6 @@ namespace Shopify.Web.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
