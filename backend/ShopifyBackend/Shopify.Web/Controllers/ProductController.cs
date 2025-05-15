421ca124ced953670f8403eb834d56a3b5394160
Vamsi <21a91a05g5@aec.edu.in>
2025-05-15 16:03:47 +0530

    Added Authorize to pages

--- backend/ShopifyBackend/Shopify.Web/Controllers/ProductController.cs
+++ backend/ShopifyBackend/Shopify.Web/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Shopify.Web.Controllers
 {
+    [Authorize]
     public class ProductController : Controller
     {
         public IActionResult Add()
