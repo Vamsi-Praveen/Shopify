fe64731c67c977a64744b14de8ffc637020b55f3
TSRCHARAN <tsr.charan@gmail.com>
2025-05-22 15:08:20 +0530

    Added Categories UI

---
+++ backend/ShopifyBackend/Shopify.Web/Models/CategoriesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Shopify.Core.Entities;
+
+namespace Shopify.Web.Models
+{
+    public class CategoriesViewModel
+    {
+        public IEnumerable<Category> Categories;
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public IFormFile? ImageFile { get; set; }
+    }
+}
